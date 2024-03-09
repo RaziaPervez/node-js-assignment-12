00627b821387e530017ac3277647a03223b42e00
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 10:02:58 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+//project=12
+
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let friendName = ["Maheen","Nadia","Isha","Muskan"];
+friendName.forEach(friendName=>console.log(`hello ${friendName},How are you?`));
